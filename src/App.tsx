c59f50865290ad01e5092bad788da9b2ffcf3c02
dkh215 <danielkhunter@gmail.com>
2020-03-21 18:58:15 -0400

    Integrate Howler library

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Howl} from 'howler';
+
 import './App.css';
 
+const PUBLIC_URL = process.env.PUBLIC_URL;
+
+const CLAP_SAMPLE = `${PUBLIC_URL}/samples/clap.mp3`;
+const HH_SAMPLE = `${PUBLIC_URL}/samples/hh.mp3`;
+
+// keycodes
+const J_KEYCODE = 74;
+const SPACEBAR_KEYCODE = 32;
+
 function App() {
+  const clap = new Howl({
+    src: [CLAP_SAMPLE]
+  });
+
+  const hh = new Howl({
+    src: [HH_SAMPLE]
+  })
+
+  function handleKeydown(e: any) {
+    const key = e.which;
+
+    if (key === J_KEYCODE) {
+      clap.play();
+    }
+
+    if (key === SPACEBAR_KEYCODE) {
+      hh.play();
+    }
+  }
+
+  React.useEffect(() => {
+    document.addEventListener('keydown', handleKeydown);
+
+    return function cleanup() {
+      document.removeEventListener('keydown', handleKeydown);
+    }
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Web Audio Sandbox</h1>
     </div>
   );
 }
